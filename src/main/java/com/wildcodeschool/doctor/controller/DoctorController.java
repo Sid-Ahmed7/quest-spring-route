e681aa65201f7a66a5140f129fe8e7f337745247
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-06-17 12:14:07 +0200

    Update method doctor

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -4,14 +4,22 @@ import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number >= 1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
     }
 }
